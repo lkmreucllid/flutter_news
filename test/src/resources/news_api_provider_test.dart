91bd13e5d8bb0b3e2449ab8f7a3a8549be805ce9
lkmreucllid <lkmreucllid01@gmail.com>
2020-09-22 10:25:18 +0530

    testing using test.dart

--- test/src/resources/news_api_provider_test.dart
+++ test/src/resources/news_api_provider_test.dart
@@ -5,12 +5,27 @@ import 'package:http/http.dart';
 import 'package:http/testing.dart';
 
 void main() {
-  test('fetchTopIds returns a list of Ids', () {
+  test('fetchTopIds returns a list of Ids', () async {
     //setup of Test cases
-
-    final sum = 1 + 3;
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient((request) async {
+      return Response(json.encode([1, 2, 3, 4]), 200);
+    });
+    final ids = await newsApi.fetchTopIds();
 
     //expectations
-    expect(sum, 4);
+    expect(ids, [1, 2, 3, 4]);
+  });
+
+  test('fetchItem returns a item model', () async {
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient((request) async {
+      final jsonMap = {'id': 123};
+      return Response(json.encode(jsonMap), 200);
+    });
+
+    final item = await newsApi.fetchItem(999);
+
+    expect(item.id, 123);
   });
 }
